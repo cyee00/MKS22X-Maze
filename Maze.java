1738d5c0b3ed0db14cef5e96ca56f46afd962507
cyee00 <cyee00@stuy.edu>
2019-02-27 11:41:22 -0500

    constructor

--- Maze.java
+++ Maze.java
@@ -19,13 +19,11 @@ public class Maze{
       }
     }
     maze = new char[rows][cols]; //set dimensions of the maze
-  }
 
-  public static void main(String[] args){
-    File text = new File(args[0]);
-    try{
+    //actually filling out the array with maze info
+    File text = new File(filename);
       Scanner file = new Scanner(text);
-      int r = 0;
+      int r=0;
       while (file.hasNextLine()){
         String line = file.nextLine();
         for (int i=0;i<line.length();i++){
@@ -33,6 +31,11 @@ public class Maze{
         }
         r++; //move on to next row
       }
+  }
+
+  public static void main(String[] args){
+    try{
+      Maze maze=new Maze(args[0]);
       System.out.println(maze.toString());
     }catch(FileNotFoundException e){
       System.out.println("File not found :(");
