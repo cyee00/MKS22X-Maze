2e9365e7a94b085790feb4678b9e3272e6de4127
cyee00 <cyee00@stuy.edu>
2019-02-27 11:32:06 -0500

    Day2

--- Maze.java
+++ Maze.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 
 public class Maze{
 
-  private char[] characters;
+  private char[][] characters;
 
   public static void main(String[] args){
     File text = new File(args[0]);
     try{
       Scanner file = new Scanner(text);
+      int r = 0;
       while (file.hasNextLine()){
         String line = file.nextLine();
-        System.out.println(line);
+        for (int i=0;i<line.length();i++){
+          characters[r][i]=line.charAt(i);
+        }
+        r++; //move on to next row
       }
+      System.out.println(characters.toString());
     }catch(FileNotFoundException e){
       System.out.println("File not found :(");
     }
